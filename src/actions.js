622e804892bc89f6ef54d585a79a2833b6684941
biccs dev <biccs@3.land>
2024-10-06 18:51:29 -0600

    cors finals

--- src/actions.js
+++ src/actions.js
@@ -20,18 +20,18 @@ app.options('*', cors(
         "optionsSuccessStatus": 204
     }
 ));
-app.use((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids');
-    res.setHeader('Access-Control-Expose-Headers', 'X-Action-Version, X-Blockchain-Ids');
-    res.setHeader('X-Blockchain-Ids', 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp');
-    res.setHeader('X-Action-Version', '0.1');
-    res.setHeader('Content-Encoding', 'compress');
-    res.setHeader('Content-Type', 'application/json');
-});
-// app.use(actionCorsMiddleware());
+// app.use((req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids');
+//     res.setHeader('Access-Control-Expose-Headers', 'X-Action-Version, X-Blockchain-Ids');
+//     res.setHeader('X-Blockchain-Ids', 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp');
+//     res.setHeader('X-Action-Version', '0.1');
+//     res.setHeader('Content-Encoding', 'compress');
+//     res.setHeader('Content-Type', 'application/json');
+// });
+app.use(actionCorsMiddleware());
 
 // console.log(app)
 
