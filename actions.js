2e3d95ef7e54bea36729855c3d29a7352a77d0c0
biccs dev <biccs@3.land>
2024-10-06 21:47:29 -0600

    copnfig

---
+++ actions.js
@@ -0,0 +1,47 @@
+import { rules, host } from './config.js';
+
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { promises as fs } from 'fs';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { actionCorsMiddleware } from '@solana/actions';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.options('*', cors(
+    {
+        "methods": ["GET,PUT,POST,OPTIONS"],
+        "allowedHeaders": ['Content-Type, Authorization, Content-Encoding, Accept-Encoding'],
+        "allowedHeaders": ['Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids'],
+        "exposeHeaders": ['X-Action-Version, X-Blockchain-Ids'],
+        "preflightContinue": true,
+        "optionsSuccessStatus": 204
+    }
+));
+app.use(actionCorsMiddleware());
+
+import { test } from './actions/test.js';
+app.use("/", test);
+
+app.get("/actions.json", async (req, res) => {
+    try {
+        const filePath = join(__dirname, 'actions.json');
+        const data = await fs.readFile(filePath, 'utf8');
+        const jsonData = JSON.parse(data);
+        res.json(jsonData);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ actions/test.js
@@ -0,0 +1,55 @@
+import { host } from '../config.js';
+// import { rpc, host } from '../config.js';
+import Express from 'express';
+import { MEMO_PROGRAM_ID } from "@solana/actions";
+import { Connection, PublicKey, Keypair, Transaction, ComputeBudgetProgram, TransactionInstruction, clusterApiUrl } from '@solana/web3.js';
+
+const test = Express.Router();
+const connection = new Connection(clusterApiUrl('devnet'), "confirmed");
+
+const MemoTx = async (pubkey) => {
+    const transaction = new Transaction();
+    transaction.feePayer = pubkey;
+    transaction.add(ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 1000 }),
+        new TransactionInstruction({ programId: new PublicKey(MEMO_PROGRAM_ID), data: Buffer.from("Memo!", "utf8"), keys: [] }));
+    const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+    transaction.recentBlockhash = blockhash;
+    transaction.lastValidBlockHeight = lastValidBlockHeight;
+    const serializedTransaction = transaction.serialize({ requireAllSignatures: false, verifySignatures: false });
+    return serializedTransaction.toString('base64');
+};
+
+test.get('/api/actions/blink', (req, res) => {
+    try {
+        const jsonData = {
+            type: "action",
+            icon: 'https://pbs.twimg.com/media/GTDGt3wbAAAmYQ5?format=jpg&name=large',
+            title: 'Hello World',
+            description: "This is my first blink",
+            label: "Blink",
+            links: {
+                actions: [{
+                    label: "click",
+                    href: host + "/api/actions/send"
+                }],
+            },
+            disabled: false,
+            error: { message: "This blink is not implemented yet!" }
+        };
+        res.json(jsonData);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: error });
+    }
+});
+
+test.post('/api/actions/send', async (req, res) => {
+    const wallet = Keypair.generate();
+    const pubkey = wallet.publicKey;
+    const tx = await MemoTx(pubkey);
+    const payload = { transaction: tx, message: 'hello' }
+    console.log(payload);
+    res.json(payload);
+});
+
+export { test }; 
