98b2f125f55700fd8672f0fb2c28ed4fb125bc81
biccs dev <biccs@3.land>
2024-10-06 18:32:02 -0600

    corsss

--- src/actions.js
+++ src/actions.js
@@ -10,28 +10,28 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-// app.options('*', cors(
-//     {
-//         "methods": ["GET,PUT,POST,OPTIONS"],
-//         "allowedHeaders": ['Content-Type, Authorization, Content-Encoding, Accept-Encoding'],
-//         "allowedHeaders": ['Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids'],
-//         "exposeHeaders": ['X-Action-Version, X-Blockchain-Ids'],
-//         "preflightContinue": true,
-//         "optionsSuccessStatus": 204
-//     }
-// ));
-// app.use((req, res) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding');
-//     // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids');
-//     // res.setHeader('Access-Control-Expose-Headers', 'X-Action-Version, X-Blockchain-Ids'),
-//     // res.setHeader('X-Blockchain-Ids', 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp');
-//     // res.setHeader('X-Action-Version', '');
-//     res.setHeader('Content-Encoding', 'compress');
-//     res.setHeader('Content-Type', 'application/json');
-// });
-app.use(actionCorsMiddleware());
+app.options('*', cors(
+    {
+        "methods": ["GET,PUT,POST,OPTIONS"],
+        "allowedHeaders": ['Content-Type, Authorization, Content-Encoding, Accept-Encoding'],
+        "allowedHeaders": ['Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids'],
+        "exposeHeaders": ['X-Action-Version, X-Blockchain-Ids'],
+        "preflightContinue": true,
+        "optionsSuccessStatus": 204
+    }
+));
+app.use((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Action-Version, X-Blockchain-Ids'),
+        res.setHeader('X-Blockchain-Ids', 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp');
+    res.setHeader('X-Action-Version', '');
+    res.setHeader('Content-Encoding', 'compress');
+    res.setHeader('Content-Type', 'application/json');
+});
+// app.use(actionCorsMiddleware());
 
 // console.log(app)
 
